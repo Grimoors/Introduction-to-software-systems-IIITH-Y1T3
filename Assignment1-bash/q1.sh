180121ec3d07ba54dc1d0dc8b1b66b5cc30c5646
Vivek Mathur <71319149+Grimoors@users.noreply.github.com>
2021-06-01 13:15:00 +0530

    Init-Commit

---
+++ Assignment1-bash/q1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "This is the Program for Q1"
+echo "START"
+echo "END"
---
+++ Class-Follow-Bash/args.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo
+if test "$1"=""
+    then
+        echo "No input in input 1"
+        exit
+fi
+if  test "$2" = ""
+    then
+        echo "No input in input 2"
+        exit
+fi
+clear
+echo
+echo ""
