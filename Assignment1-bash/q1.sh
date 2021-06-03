e3e658cb0723166898dd1e0ea955370c1e7e42f2
Vivek Mathur <71319149+Grimoors@users.noreply.github.com>
2021-06-03 15:32:49 +0530

    Did q1.sh

--- Assignment1-bash/q1.sh
+++ Assignment1-bash/q1.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 echo "This is the Program for Q1"
 echo "START"
+echo
+
+declare -l inputString
+read -p "Enter var inputString: " inputString
+
+echo "Input = " $inputString
+echo
+echo "Starting the Processing"
+
+declare -i length
+length=${#inputString}
+echo "Length of '$inputString' is $length"   
+echo
+
+declare -l substr
+substr="${inputString:0:4}"
+echo "The First four or lesser characters are: '${substr}' "
+echo "Total characters in Substring: ${#substr} "  
+echo
+
+declare -i hashLength
+hashLength=$length-4
+declare -l output
+if [ ${#inputString} -gt 4 ]
+then
+    echo "The size of the string is greater that 4, We need to insert [[${hashLength}]] Hashes at the end "
+    output=$substr
+        for ((i=0;i<$hashLength;i++))
+        do
+            output+='#'
+        done
+
+else
+    echo "The size of the string is less than or equal to 4, this is our final Answer"
+    output=$substr
+fi 
+
+echo
+echo "Processed"
+echo
+echo "Printing output"
+echo "Output = $output"
+
 echo "END"
---
+++ Assignment1-bash/q2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "This is the Program for Q2"
+echo "START"
+declare -l inputString
+declare -l output
+echo
+echo "Starting the Processing"
+echo
+echo "Processed"
+echo
+echo "Printing output"
+echo "Output = $output"
+
+echo "END"
